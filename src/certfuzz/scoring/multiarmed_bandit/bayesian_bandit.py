907d27765b76304cdc0a48468d73006c6e2054ff
pwndorei <lkjuio3865@naver.com>
2024-05-22 15:29:28 +0900

    revert method name: next to __next__

--- src/certfuzz/scoring/multiarmed_bandit/bayesian_bandit.py
+++ src/certfuzz/scoring/multiarmed_bandit/bayesian_bandit.py
@@ -27,7 +27,7 @@ class BayesianMultiArmedBandit(MultiArmedBanditBase):
     def _next_key(self):
         return weighted_choice(self._scaled_scores())
 
-    def next(self):
+    def __next__(self):
         # if there aren't any arms, we're done.
         if not len(self.arms):
             raise StopIteration
